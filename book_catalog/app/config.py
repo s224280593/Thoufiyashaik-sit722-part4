a931c0e6e92350ad5f71ba6e463af12d7f5d7305
s224280593 <162409765+s224280593@users.noreply.github.com>
2024-10-03 00:35:01 +1000

    initial commit

--- book_catalog/app/config.py
+++ book_catalog/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://thoufiya_sit722_part4_user:A43GVww31L500UdUUncAKwRqjPBY2nmx@dpg-crr1a52j1k6c73e80jgg-a.oregon-postgres.render.com/thoufiya_sit722_part4')
